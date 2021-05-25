caaccb946c98c51fab15e946fde27963f1825cf5
shaggytaro <75380175+shaggytaro@users.noreply.github.com>
2021-05-25 16:45:11 -0500

    Second Commit

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -11,8 +11,8 @@ function Contact() {
         <Container fluid>
           <h1 className="display-3">My Contact Details</h1>
           <p className="lead">Here are my contact details </p>
-          <p className="lead">Email : sachin@wynisco.com my email </p>
-          <p className="lead">Phone : 732 874 0188 </p>
+          <p className="lead">Email : pcprashant3@gmail.com my email </p>
+          <p className="lead">Phone : 573 200 1109 </p>
           <p className="lead">Typical Wake up / Sleep Hours : Other than bootcamps. I will available morning 7AM to 11AM EST via slack or whatsapp Changed  </p>
         </Container>
       </Jumbotron>
